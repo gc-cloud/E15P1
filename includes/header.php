f49ab14fd49f3aa2650d802c1d18e6008be95eae
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-09-13 15:48:20 -0500

    Fix W3C validation messages

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,6 @@
         <h1 >Gerardo Castaneda</h1>
         <h2 >Dynamic Web Apps Portfolio</h2>
       </div>
-        <img src="images/gerardo.jpg"  alt="gerardo" class="photo">
+        <img src="images/gerardo.jpg"  alt="gerardo_and_mac" class="photo">
       </div>
     </header>
